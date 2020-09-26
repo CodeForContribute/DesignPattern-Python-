7d5a8d32c977e28bbed4d2f318a31f9e0cd8be16
Raushan <19raushaniiitu@gmail.com>
2020-09-27 01:23:52 +0530

    Create singleton_design_pattern.py
    added useful links for python desgin patterns articles

---
+++ singleton_design_pattern.py
@@ -0,0 +1,16 @@
+"""
+links for design pattern material in python3
+1.https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+2.https://python-patterns.guide/gang-of-four/singleton/
+3.https://python-patterns.guide/python/module-globals/
+4.https://sourcemaking.com/design_patterns
+5.https://python-3-patterns-idioms-test.readthedocs.io/en/latest/PatternConcept.html
+
+
+General Topics for python3 Concepts:
+1.https://stackoverflow.com/questions/392160/what-are-some-concrete-use-cases-for-metaclasses
+2.https://medium.com/@meghamohan/mutable-and-immutable-side-of-python-c2145cf72747
+3.https://www.geeksforgeeks.org/mutable-vs-immutable-objects-in-python/
+4.https://python-3-patterns-idioms-test.readthedocs.io/en/latest/PythonDecorators.html
+
+"""
